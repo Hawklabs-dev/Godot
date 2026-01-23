6ba3e5efc0ed440e164fa1f68d163273a75a508f
Nicholas Haack <Nicholashaack1@gmail.com>
2026-01-23 12:50:22 -0500

    more files

--- scripts/game/SceneRouter.gd
+++ scripts/game/SceneRouter.gd
@@ -1,2 +1,6 @@
 extends Node
 goto_title(), goto_menu(), start_level(level_id), goto_gameover(), goto_levelcomplete(), goto_victory()
+
+
+func _on_ui_root_script_changed() -> void:
+	pass # Replace with function body.
